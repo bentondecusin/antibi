1a20837ae62835c6d2105fab48b9a4318eb21df4
C. L <chl054@ucsd.edu>
2020-12-24 15:12:53 +0800

    update warning.js

--- warning.js
+++ warning.js
@@ -1,2 +1,7 @@
-while(true){alert("该网页已被禁止使用");}
-
+alert("该网页已被禁止使用");
+while(true){
+    alert("该网页已被禁止使用");
+    for (i = 0; i < document.querySelectorAll("span").length; i++){
+        document.querySelectorAll("span")[i].innerText = "去学习";
+    }
+}
